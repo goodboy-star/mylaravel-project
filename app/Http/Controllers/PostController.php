6a2941f20b0644fc678c90a341705a256c793e56
unknown <jenisben0501@gmail.com>
2024-02-02 16:42:32 -0800

    first commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -24,7 +25,13 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+          ]);
+          Post::create($request->all());
+          return redirect()->route('posts.index')
+            ->with('success','Post created successfully.');
     }
 
     /**
@@ -35,7 +42,8 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+    return view('posts.show', compact('post'));
     }
 
     /**
@@ -47,7 +55,14 @@ class PostController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+          ]);
+          $post = Post::find($id);
+          $post->update($request->all());
+          return redirect()->route('posts.index')
+            ->with('success', 'Post updated successfully.');
     }
 
     /**
@@ -58,6 +73,20 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect()->route('posts.index')
+          ->with('success', 'Post deleted successfully');
     }
+
+    public function create()
+    {
+      return view('posts.create');
+    }
+
+    public function edit($id)
+  {
+    $post = Post::find($id);
+    return view('posts.edit', compact('post'));
+  }
 }
